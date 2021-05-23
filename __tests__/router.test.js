6671b16bff351e066dba280932866e29e32f5bb4
yhkim5756 <yok011@ucsd.edu>
2021-05-22 23:45:46 -0700

    Update router.test.js

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,24 @@
  * @jest-environment jsdom
  */
 
+
+
+import { pushToHistory } from '../scripts/router.js';
+const router = {};
+
+ describe('length test', () => {
+    test('settings', () => {
+        pushToHistory('settings'); 
+        expect(history.length).toBe(2);
+    });
+  
+    test('entry', () => {
+        pushToHistory('entry', 1);  
+        expect(history.length).toBe(3);
+    });
+
+    test('default', () => {
+        pushToHistory('default');
+        expect(history.length).toBe(4);
+      });
+  });
